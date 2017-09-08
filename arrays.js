4ba7e5350c87a12a0de048b575b2e3e955724f0c
Steve Meservey <stevendmeservey@gmail.com>
2017-09-08 20:07:25 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
-function addElementToEndOfArray(array, element) {
+function addElementToBeginningOfArray(array, element) {
 return ["milky way", ...chocolateBars]
 }
