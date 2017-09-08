90b1270092dab062d81e4c739a94e06d55e9804d
Steve Meservey <stevendmeservey@gmail.com>
2017-09-08 20:04:06 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
-function addElementToEndOfArray(array, element){
-return [element, ...array]
+function addElementToEndOfArray(array, element) {
+return [element,...array]
 }
